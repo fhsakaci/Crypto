c0197a33a7235a3e79cbb6195a8b730174d476b9
fhsakaci <furkan.sakaci@ogr.sakarya.edu.tr>
2019-01-03 17:04:52 +0300

    Initial

---
+++ TwineProjectCrypto/TwineProjectCrypto/MainForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Fatih
+ * Date: 13.12.2018
+ * Time: 19:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace TwineProjectCrypto
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox Key;
+		private System.Windows.Forms.TextBox PlainText;
+		private System.Windows.Forms.Button Crypto;
+		private System.Windows.Forms.Label CryptoText;
+		private System.Windows.Forms.Label plaint;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.Key = new System.Windows.Forms.TextBox();
+			this.PlainText = new System.Windows.Forms.TextBox();
+			this.Crypto = new System.Windows.Forms.Button();
+			this.CryptoText = new System.Windows.Forms.Label();
+			this.plaint = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.Control;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.Key);
+			this.panel1.Controls.Add(this.PlainText);
+			this.panel1.Controls.Add(this.Crypto);
+			this.panel1.Location = new System.Drawing.Point(325, 187);
+			this.panel1.Margin = new System.Windows.Forms.Padding(2);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(330, 145);
+			this.panel1.TabIndex = 0;
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(67, 9);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(191, 29);
+			this.label5.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(67, 65);
+			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(75, 19);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Plain Text:";
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(67, 43);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 19);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Key:";
+			// 
+			// Key
+			// 
+			this.Key.Location = new System.Drawing.Point(146, 40);
+			this.Key.Margin = new System.Windows.Forms.Padding(2);
+			this.Key.Name = "Key";
+			this.Key.Size = new System.Drawing.Size(146, 20);
+			this.Key.TabIndex = 2;
+			// 
+			// PlainText
+			// 
+			this.PlainText.Location = new System.Drawing.Point(146, 64);
+			this.PlainText.Margin = new System.Windows.Forms.Padding(2);
+			this.PlainText.Multiline = true;
+			this.PlainText.Name = "PlainText";
+			this.PlainText.Size = new System.Drawing.Size(146, 46);
+			this.PlainText.TabIndex = 1;
+			// 
+			// Crypto
+			// 
+			this.Crypto.Location = new System.Drawing.Point(175, 113);
+			this.Crypto.Margin = new System.Windows.Forms.Padding(2);
+			this.Crypto.Name = "Crypto";
+			this.Crypto.Size = new System.Drawing.Size(117, 23);
+			this.Crypto.TabIndex = 0;
+			this.Crypto.Text = "Encrypt-Decrypt";
+			this.Crypto.UseVisualStyleBackColor = true;
+			this.Crypto.Click += new System.EventHandler(this.CryptoClick);
+			// 
+			// CryptoText
+			// 
+			this.CryptoText.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.CryptoText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CryptoText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.CryptoText.Location = new System.Drawing.Point(51, 9);
+			this.CryptoText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.CryptoText.Name = "CryptoText";
+			this.CryptoText.Size = new System.Drawing.Size(300, 138);
+			this.CryptoText.TabIndex = 1;
+			// 
+			// plaint
+			// 
+			this.plaint.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.plaint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.plaint.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.plaint.Location = new System.Drawing.Point(355, 9);
+			this.plaint.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.plaint.Name = "plaint";
+			this.plaint.Size = new System.Drawing.Size(300, 138);
+			this.plaint.TabIndex = 2;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(51, 302);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(212, 19);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Furkan Hasan SAKACI";
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(163, 151);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(65, 23);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "Crypto";
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(455, 151);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(89, 23);
+			this.label7.TabIndex = 6;
+			this.label7.Text = "Decrypto";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(683, 353);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.plaint);
+			this.Controls.Add(this.CryptoText);
+			this.Controls.Add(this.panel1);
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "MainForm";
+			this.Text = "TwineProjectCrypto";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ TwineProjectCrypto/TwineProjectCrypto/MainForm.cs
@@ -0,0 +1,214 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Fatih
+ * Date: 13.12.2018
+ * Time: 19:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text;
+
+namespace TwineProjectCrypto
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		byte[] sbox={0x0C, 0x00, 0x0F, 0x0A, 0x02, 0x0B, 0x09, 0x05, 0x08, 0x03, 0x0D, 0x07, 0x01, 0x0E, 0x06, 0x04};
+		int[] shuf={5, 0, 1, 4, 7, 12, 3, 8, 13, 6, 9, 2, 15, 10, 11, 14};
+		int[] shufinv={1, 2, 11, 6, 3, 0, 9, 4, 7, 10, 13, 14, 5, 8, 15, 12};
+		byte[] roundconst={
+		0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x03, 0x06, 0x0c, 0x18, 0x30, 0x23, 0x05, 0x0a, 0x14, 0x28, 0x13, 0x26,
+		0x0f, 0x1e, 0x3c, 0x3b, 0x35, 0x29, 0x11, 0x22, 0x07, 0x0e, 0x1c, 0x38, 0x33, 0x25, 0x09, 0x12, 0x24, 0x0b};
+		byte[,] rk=new byte[36,8];
+		byte[] crt_byte=new byte[8];
+		byte[] dec_byte=new byte[8];
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void CryptoClick(object sender, EventArgs e)
+		{
+			string k=Key.Text;
+			string[] plainArray = new string[100];
+
+			
+			if(k.Length==10)
+			{
+				label5.Text="";
+				New(k);
+				string plaintxt=PlainText.Text;
+				
+				int counter=0;
+				for(int i=0;i<plaintxt.Length;i+=8)
+				{
+					if(plaintxt.Length-i<=8)
+						plainArray[counter]=plaintxt.Substring(i,plaintxt.Length-i);
+					else
+						plainArray[counter]=plaintxt.Substring(i,8);		
+					counter+=1;
+				}
+				string s="",cs="";
+				for(int i=0;i<counter;i++)
+				{
+					byte[] cryptotext=Encrypt(plainArray[i]);
+					s+=Encoding.ASCII.GetString(cryptotext);
+					cs+=Decrypt(cryptotext);
+				}
+				CryptoText.Text=s;
+				plaint.Text=cs;
+			}
+			else
+			{
+				label5.Text="Key length should be 10";
+				label5.ForeColor=Color.Red;
+			}
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		
+		
+		void New(string NewKEY)
+		{
+			byte[] wk=new byte[20];
+			byte[] keyBytes=Encoding.ASCII.GetBytes(NewKEY);
+			for(int i=0;i<keyBytes.Length;i++)
+			{
+				wk[2*i] = Convert.ToByte(keyBytes[i] >> 4);
+				wk[2*i+1] = Convert.ToByte(keyBytes[i] & 0x0f);
+			}
+			for(int i=0;i<35;i++)
+			{
+				rk[i,0] = wk[1];
+				rk[i,1] = wk[3];
+				rk[i,2] = wk[4];
+				rk[i,3] = wk[6];
+				rk[i,4] = wk[13];
+				rk[i,5] = wk[14];
+				rk[i,6] = wk[15];
+				rk[i,7] = wk[16];
+				wk[1] ^= sbox[wk[0]];
+				wk[4] ^= sbox[wk[16]];
+				wk[7] ^= Convert.ToByte(roundconst[i] >> 3);
+				wk[19] ^= Convert.ToByte(roundconst[i] & 7);
+				byte tmp0=wk[0],tmp1=wk[1],tmp2=wk[2],tmp3=wk[3];
+				int fourj;
+				for (int j = 0; j < 4; j++) {
+					fourj = j * 4;
+					wk[fourj] = wk[fourj+4];
+					wk[fourj+1] = wk[fourj+5];
+					wk[fourj+2] = wk[fourj+6];
+					wk[fourj+3] = wk[fourj+7];
+				}
+				wk[16] = tmp1;
+				wk[17] = tmp2;
+				wk[18] = tmp3;
+				wk[19] = tmp0;
+			}
+			rk[35,0] = wk[1];
+			rk[35,1] = wk[3];
+			rk[35,2] = wk[4];
+			rk[35,3] = wk[6];
+			rk[35,4] = wk[13];
+			rk[35,5] = wk[14];
+			rk[35,6] = wk[15];
+			rk[35,7] = wk[16];
+		}
+		
+		byte[] Encrypt(string plaintxt)
+		{
+			byte[] x=new byte[16];
+			byte[] plaintxtByte=Encoding.ASCII.GetBytes(plaintxt);
+			
+			for (int i= 0; i < plaintxtByte.Length; i++) {
+				x[2*i] = Convert.ToByte(plaintxtByte[i] >> 4);
+				x[2*i+1] = Convert.ToByte(plaintxtByte[i] & 0x0f);
+			}
+			
+			for (int i = 0; i < 35; i++) 
+			{
+				for (int j = 0; j < 8; j++) 
+				{
+					x[2*j+1] ^= sbox[x[2*j]^rk[i,j]];
+				}		
+	
+				byte[] xnext=new byte[16];
+				for (int h = 0; h < 16; h++) 
+				{
+					xnext[shuf[h]] = x[h];
+				}
+				x = xnext;
+			}
+			// last round
+
+		for (int j = 0; j < 8; j++) 
+		{
+			x[2*j+1] ^= sbox[x[2*j]^rk[35,j]];
+		}
+		byte[] dst=new byte[8];
+		for (int i = 0; i < 8; i++) 
+		{
+			dst[i] = Convert.ToByte((x[2*i] <<4) | x[2*i+1]);
+		}
+		crt_byte=dst;
+		return dst;
+		}
+		string Decrypt(byte[] plaintxtByte)
+		{
+			byte[] x=new byte[16];
+			dec_byte=plaintxtByte;
+			for (int i= 0; i < plaintxtByte.Length; i++) {
+				x[2*i] = Convert.ToByte(plaintxtByte[i] >> 4);
+				x[2*i+1] = Convert.ToByte(plaintxtByte[i] & 0x0f);
+			}
+			for (int i = 35; i >= 1; i--)
+			{
+				for (int j = 0; j < 8; j++)
+				{
+					x[2*j+1] ^= sbox[x[2*j]^rk[i,j]];
+				}
+		
+				byte[] xnext=new byte[16];
+				for (int h = 0 ;h < 16; h++)
+				{
+					xnext[shufinv[h]] = x[h];
+				}
+				x = xnext;
+			}
+			
+			for (int j = 0; j < 8; j++) 
+			{
+				x[2*j+1] ^= sbox[x[2*j]^rk[0,j]];
+			}
+			byte[] dst=new byte[8];
+			for (int i = 0; i < 8; i++) 
+			{
+				dst[i] = Convert.ToByte((x[2*i] << 4) | x[2*i+1]);
+			}
+			
+			return Encoding.ASCII.GetString(dst);
+		}
+		void Label3Click(object sender, EventArgs e)
+		{
+	
+		}
+		
+	}
+}
